ef5c602d907662d35241764f8577b962a3d2d10e
Yasaman K <yasaman@m.lo.agency>
2021-11-29 13:18:37 +0330

    fix-bug: setchange

--- src/components/admin/entityForm.js
+++ src/components/admin/entityForm.js
@@ -1,21 +1,16 @@
 import config from "../../services/config";
 import CrudProvider, { useCRUD } from "../providers/crud.provider";
 import { title } from 'case'
-import { useParams } from "react-router";
+import { Navigate, useParams } from "react-router";
 
 const EntityForm = ({ entityName, actionName, id }) => {
-	// console.log("show id", id);
-	// console.log("id entity",id);
-	// const {id}= useParams()
 	const crud = useCRUD();
-	const fields = Object.keys(config.entities[entityName].fields).map(field => {
+	const Updatedfields = Object.keys(config.entities[entityName].fields).map(field => {
 		
 		// console.log(Object.values(crud.tableValue)[id][field]);
 		let field2 = ''
 		if(actionName!="create"){
-			console.log(id);
 			field2 = Object.values(crud.tableValue)[id][field]
-
 		}
 	
 
@@ -44,74 +39,54 @@ const EntityForm = ({ entityName, actionName, id }) => {
 		}
 	});
 
-	// const updateFields = crud.tableValue && Object.values(crud.tableValue).map((field, id) => {
-	// 	console.log(Object.values(crud.tableValue)[id]);
+	// if (actionName == "create") {
 	// 	return (
 	// 		<>
-	// 			<div className="relative">
 
-	// 				<div  key={id}>{Object.values(field).map((entity, id) => {
-	// 					// console.log(entity);
-	// 					return (<>
-	// 						<input className="w-96 pr-40 mr-8 bg-gray-300" placeholder={title(entity)}
-	// 						 key={id}></input>
-	// 					</>)
-	// 				})}</div>
-	// 			</div>
+	// 			<form className="formCrud border border-black rounded-lg p-5" onSubmit={crud.create}>
+	// 				{Updatedfields}
+
+	// 				<button type="submit">{actionName}</button>
+	// 			</form>
 	// 		</>
 	// 	)
-	// })
-	// console.log(Object.values(crud.tableValue)[id]);
-	// const updateFields = crud.tableValue && Object.values(Object.values(crud.tableValue)[id]).map((field, id) => {
-	// 	return Object.keys(config.entities[entityName].fields).map(field1 => {
 
-	// 		return <input name={field1} key={id} className="w-96 pr-40 mr-8 bg-gray-300" placeholder={title(field)} />
-	// 	})
-	// })
-	console.log("iddddd", id);
+	// }
+	// else {
+	// 	return (
+	// 		<>
+	// 			<form className="formCrud border border-black rounded-lg p-5" onSubmit={crud.edit(id)}>
+	// 				{Updatedfields}
+	// 				<button type="submit">{actionName}</button>
+	// 			</form>
+	// 		</>
+	// 	)
+	// }
+	// crud[actionName](id)
 
-	if (actionName == "create") {
-		return (
-			<>
+	const handleSubmit = (event) =>{
+		event.preventDefault()
+        const form = new FormData(event.target)
+        const values = Object.keys(config.entities[entityName].fields).reduce((values, field) => {
+            const value = form.get(field)
+            values[field] = value
+            return values
+        }, {})
+		return crud[actionName](values,id)
 
-				<form className="formCrud border border-black rounded-lg p-5" onSubmit={crud.create}>
-					{fields}
-
-					<button type="submit">{actionName}</button>
-				</form>
-			</>
-		)
 
 	}
-	else {
-		return (
-			<>
-				<form className="formCrud border border-black rounded-lg p-5" onSubmit={crud.edit(id)}>
-					{fields}
-					<button type="submit">{actionName}</button>
-				</form>
-			</>
-		)
-	}
 
-	// return (
-	// 	<>
+	return (
+		<>
+			<form className="formCrud border border-black rounded-lg p-5" onSubmit={handleSubmit}>
+				{Updatedfields}
 
-	// 		<form className="formCrud border border-black rounded-lg p-5" onSubmit={crud.create}>
-	// 			{fields}
+				<button type="submit">{actionName}</button>
+			</form>
 
-	// 			<button type="submit">{actionName}</button>
-	// 		</form>
-
-
-	// 		<form className="formCrud border border-black rounded-lg p-5" onSubmit={crud.edit(id)}>
-	// 			{fields}
-	// 			<button type="submit">{actionName}</button>
-	// 		</form>
-
-
-	// 	</>
-	// )
+		</>
+	)
 };
 
 export default EntityForm;
--- src/components/providers/crud.provider.js
+++ src/components/providers/crud.provider.js
@@ -11,108 +11,32 @@ const CrudProvider = ({ children }) => {
 
     const { entityName } = useParams();
     const [tableValue, setTableValue] = useState([])
-    // const [change1, setChange1] = useState(true);
-    const [change, setChange] = useState(true);
-    const [change1, setChange1] = useState(true);
 
-    const create = (event) => {
-        event.preventDefault()
-        console.log("what");
-        const form = new FormData(event.target)
-        const values = Object.keys(config.entities[entityName].fields).reduce((values, field) => {
-            const value = form.get(field)
-            values[field] = value
-            return values
-        }, {})
+    const [change, setChange] = useState(0);
 
-        console.log(values);
 
+    const create = (values, id) => {
         push(ref(db, `${entityName}`), values)
             .then(() => {
                 alert("data successful")
+                setChange(change + 1)
             })
             .catch((error) => {
                 alert("unsuccessful" + error)
             })
-            // setChange1(!change1)
-        console.log("hi");
-        setChange(!change1)
+        setChange(change + 1)
     }
-
-    const edit = ((id) => {
-        return (event) => {
-            event.preventDefault()
-            const form = new FormData(event.target)
-            const values = Object.keys(config.entities[entityName].fields).reduce((values, field) => {
-                const value = form.get(field)
-                values[field] = value
-                return values
-            }, {})
-            // console.log("iddd",id);
-            console.log(values);
-            console.log(Object.keys(tableValue)[id]);
-            const idEntityName = Object.keys(tableValue)[id]
-            update(ref(db, `${entityName}` + '/' + `${idEntityName}`), values)
-                .then(() => {
-                    alert("data successful update")
-                })
-                .catch((error) => {
-                    alert("unsuccessful" + error)
-                })
-        }
-   
-    })
-    // const edit = (event) => {
-    //     event.preventDefault()
-    //     const form = new FormData(event.target)
-    //     const values = Object.keys(config.entities[entityName].fields).reduce((values, field) => {
-    //         const value = form.get(field)
-    //         values[field] = value
-    //         return values
-    //     }, {})
-    //     // console.log("iddd",id);
-    //     console.log(values);
-    //     console.log(Object.keys(tableValue)[0]);
-    //     const idEntityName = Object.keys(tableValue)[0]
-    //     update(ref(db, `${entityName}` + '/' + `${idEntityName}`), values)
-    //         .then(() => {
-    //             alert("data successful update")
-    //         })
-    //         .catch((error) => {
-    //             alert("unsuccessful" + error)
-    //         })
-    // }
-
-
-
-    const showData = () => {
-        get(child(dbRef, `${entityName}`)).then((snapshot) => {
-            if (snapshot.exists()) {
-                setTableValue(snapshot.val())
-                console.log(snapshot.val());
-            } else {
-                console.log("No data available");
-            }
-        }).catch((error) => {
-            console.error(error);
-        });
+    const edit = (values, id) => {
+        const idEntityName = Object.keys(tableValue)[id]
+        update(ref(db, `${entityName}` + '/' + `${idEntityName}`), values)
+            .then(() => {
+                alert("data successful update")
+            })
+            .catch((error) => {
+                alert("unsuccessful" + error)
+            })
+        setChange(change + 1)
     }
-
-   
-    useEffect(() => {
-        get(child(dbRef, `${entityName}`)).then((snapshot) => {
-            if (snapshot.exists()) {
-                setTableValue(snapshot.val())
-                // console.log(tableValue);
-                // console.log(snapshot.val());
-                // console.log(  Object.values(snapshot.val())  )
-            } else {
-                console.log("No data available");
-            }
-        }).catch((error) => {
-            console.error(error);
-        });
-    }, [entityName, change,change1]);
 
     const deleteData = (id) => {
         const idEntityName = Object.keys(tableValue)[id]
@@ -123,8 +47,23 @@ const CrudProvider = ({ children }) => {
             .catch(() => {
                 alert("unseccesful")
             })
-        setChange(!change)
+        setChange(change + 1)
     }
+
+    useEffect(() => {
+        get(child(dbRef, `${entityName}`)).then((data) => {
+            if (data.exists()) {
+                setChange(change+1)
+                setTableValue(data.val())         
+            } else {
+                console.log("No data available");
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+       
+    }, [entityName, change]);
+
 
     return (
         <CRUDContext.Provider
@@ -132,7 +71,6 @@ const CrudProvider = ({ children }) => {
                 create,
                 edit,
                 deleteData,
-                showData,
                 tableValue,
 
             }}
