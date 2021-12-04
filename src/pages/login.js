374dbcc44bd83583018c3657058c3e643950c4e5
ZahraZfr <Zahra.Z@m.lo.agency>
2021-12-05 01:39:08 +0330

    feat:login  ui

--- src/pages/login.js
+++ src/pages/login.js
@@ -34,18 +34,18 @@ const Login = () => {
 	};
 
 	return (
-		<div className="text-3xl text-center pt-14 ">
-			<p className="text-5xl">You must log in to view the page at {from}</p>
+		<div className="text-3xl text-center pt-14 rounded-full ">
+			<p className="text-3xl mb-20">You must log in to view the page at {from}</p>
 			<p className="text-red-500 text-base">{error}</p>
 
-			<form className="flex flex-col mt-6" onSubmit={handleSubmit}>
+			<form className="flex justify-center mx-auto rounded-2xl flex-col mt-6 w-1/3 bg-gray-200 shadow-xl" onSubmit={handleSubmit}>
 				<label className="mb-5">
-					Email: <input className="shadow-2xl bg-white w-1/5 py-5 rounded-2xl mx-8 mt-28" name="email" type="email" required />
+					Email: <input className="shadow-2xl bg-white w-3/5 py-5 rounded-2xl mx-8 mt-28 mb-2" name="email" type="email" required />
 				</label>
 				<label>
-					Password: <input className="shadow-2xl bg-white w-1/5 py-5 rounded-2xl mx-8" name="password" type="password" required />
+					Password: <input className="shadow-2xl bg-white w-3/5 py-5 rounded-2xl mx-8 mb-5" name="password" type="password" required />
 				</label>
-				<button className=" flex items-center justify-center mt-7 mx-auto border-black bg-green-400 px-36 py-5 w-32 rounded-full" type="submit" >Login</button>
+				<button className="mb-28 flex items-center justify-center mt-7 mx-auto  bg-green-400 px-36 py-5 w-32 rounded-full" type="submit" >Login</button>
 			</form>
 		</div>
 	);
